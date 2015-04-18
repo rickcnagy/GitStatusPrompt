22d541a771435403d9d8347b18c11c26cd843353
Rick Nagy <rick.c.nagy@gmail.com>
2015-04-18 02:54:37 -0400

    Update from .bashrc to .profile

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ mv ~/GitStatusPrompt/.git_status_prompt ~/.git_status_prompt
 mv ~/GitStatusPrompt/.git-prompt.sh ~/.git-prompt.sh
 mv ~/GitStatusPrompt/.git-completion.sh ~/.git-completion.sh
 
-# Add the .bash_prompt invocation to .bashrc
-echo "# Adding ~/.git_prompt_command to ~/.bashrc"
-echo >> ~/.bashrc
-echo "# Detect and display prompt in bash status" >> ~/.bashrc
-echo "# (https://github.com/br1ckb0t/GitStatusPrompt)" >> ~/.bashrc
-echo "source ~/.git_status_prompt" >> ~/.bashrc
+# Add the .bash_prompt invocation to .profile
+echo "# Adding ~/.git_prompt_command to ~/.profile"
+echo >> ~/.profile
+echo "# Detect and display prompt in bash status" >> ~/.profile
+echo "# (https://github.com/br1ckb0t/GitStatusPrompt)" >> ~/.profile
+echo "source ~/.git_status_prompt" >> ~/.profile
 
-source ~/.bashrc
+source ~/.profile
 
-echo "Installed. Please check ~/.bashrc for duplicate sourcing of ~/.git_status_prompt"
+echo "Installed. Please check ~/.profile for duplicate sourcing of ~/.git_status_prompt"
